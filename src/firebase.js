01770fee37c13e2aac16ab9697f91af75ce0d2fe
mikachi20 <mKs1m400H@gmail.com>
2023-01-19 14:14:17 +0900

    初期環境, vue3, firebase, router

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC59tQwH8q_ri32lcWoG1WDXoVQeFArSyc",
+  authDomain: "webex16-team2.firebaseapp.com",
+  projectId: "webex16-team2",
+  storageBucket: "webex16-team2.appspot.com",
+  messagingSenderId: "133220401460",
+  appId: "1:133220401460:web:c4312a2a4b00af57966561",
+  measurementId: "G-JGB787SV7W",
+  databaseURL:
+    "https://webex16-team2-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
